7c7b1386aead326987d482d795420c6437bda8e1
aserebryakov <overlapped@gmail.com>
2023-11-24 02:37:00 +0700

    Add QtHtml unit tests project.

--- example/main.cpp
+++ example/main.cpp
@@ -1,29 +1,56 @@
-#include "QHtmlWriter"
-#include "QDebug"
+#include <QtCore/QDebug>
+#include <QHtmlWriter>
 
 int main()
 {
-    QHtmlDocument doc("Welcome to Qt Html!");
-    doc.setLang("en");
-    QHtmlHead& head = doc.head();
-    QHtmlBody& body = doc.body();
+    using namespace QtHtml;
 
-    head << QHtmlMeta("utf-8");
-    head << QHtmlMeta("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no");
-    head << QHtmlMeta().setHttpEquiv(QHtmlMeta::HttpEquiv::Refresh, "3");
-    head << QHtmlStyle("{.navbar{margin-bottom:20px;}");
-    head << QHtmlLink("stylesheet", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
+    Document doc("Welcome to Qt Html!");
+    doc.setLang("en");
+    Head& head = doc.head();
+    Body& body = doc.body();
+
+    head << Meta("utf-8");
+    head << Meta("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no");
+    head << Meta().setHttpEquiv(Meta::HttpEquiv::Refresh, "3");
+    head << Style("{.navbar{margin-bottom:20px;}");
+    head << Link("stylesheet", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
 
     body.setClass("bg-light");
 
-    doc << QHtmlHeader1("Welcome to Qt Html!");
+    List navList(false, "navbar-nav mr-auto");
+    navList << (ListItem().setClass("nav-item active") << Anchor("Home", QUrl("#")).setClass("nav-link"));
+    navList << (ListItem().setClass("nav-item") << Anchor("Link", QUrl("#")).setClass("nav-link"));
+    navList << (ListItem().setClass("nav-item") << Anchor("Disabled", QUrl("#")).setClass("nav-link disabled"));
+    navList << (ListItem().setClass("nav-item dropdown")
+            << Anchor("Dropdown", QUrl("#")).setClass("nav-link dropdown-toggle").setId("dropdown01").addAttribute("data-toggle", "dropdown").addAttribute("aria-haspopup", "true").addAttribute("aria-expanded", "false")
+            << (Div("dropdown-menu").addAttribute("aria-labelledby", "dropdown01")
+            << Anchor("Action", QUrl("#")).setClass("dropdown-item")
+            << Anchor("Other", QUrl("#")).setClass("dropdown-item")));
 
-    doc << (QHtmlTable().setClass("table table-hover table-sm")
-        << QHtmlCaption("Table caption")
-        << (QHtmlTableRow() << QHtmlTableHeader("A") << QHtmlTableHeader("B"))
-        << (QHtmlTableRow() << QHtmlTableDataCell("Cell 11") << QHtmlTableDataCell("Cell 12"))
-        << (QHtmlTableRow() << QHtmlTableDataCell("Cell 21") << QHtmlTableDataCell("Cell 22"))
-        << (QHtmlTableRow() << QHtmlTableDataCell("Cell 31") << (QHtmlTableDataCell() << QHtmlAnchor("RASU", QUrl("http://www.rasu.ru")))));
+    doc << (Nav("navbar navbar-expand navbar-dark bg-dark") << Div("collapse navbar-collapse") << navList);
+
+    Div main("container");
+
+    main << Header1("Welcome to Qt Html!");
+    main << "Text directly in the body." << LineBreak();
+
+    main << Paragraph("This is the way to go for a big text in a multi-line paragraph.");
+    main << Address("CSS");
+    main << Anchor("Google", QUrl("http://google.com")).setClass("my_style");
+
+    main << (Table().setClass("table table-hover table-sm")
+         << Caption("Table caption")
+         << (TableRow() << TableHeader("A") << TableHeader("B"))
+         << (TableRow() << TableDataCell("Cell 11") << TableDataCell("Cell 12"))
+         << (TableRow() << TableDataCell("Cell 21") << TableDataCell("Cell 22"))
+         << (TableRow() << TableDataCell("Cell 31") << (TableDataCell() << Anchor("Google", QUrl("http://www.google.com")))));
+
+    main << Label("txt1", "The text") << TextArea("textform", "This is text...").setRequired().setPlaceHolder("The text").setId("txt1");
+
+    main << Small("Copyright Serebryakov A. &copy; 2011-2023");
+
+    doc << main;
 
     QString str;
     QTextStream os(&str);
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,61 +1,79 @@
-#include <QtCore/QDebug>
+#include <QtTest>
 #include <QHtmlWriter>
 
-int main()
+class QtHtmlTests : public QObject
 {
-    using namespace QtHtml;
+    Q_OBJECT
 
-    Document doc("Welcome to Qt Html!");
-    doc.setLang("en");
-    Head& head = doc.head();
-    Body& body = doc.body();
+public:
+    QtHtmlTests();
+    ~QtHtmlTests();
 
-    head << Meta("utf-8");
-    head << Meta("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no");
-    head << Meta().setHttpEquiv(Meta::HttpEquiv::Refresh, "3");
-    head << Style("{.navbar{margin-bottom:20px;}");
-    head << Link("stylesheet", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
 
-    body.setClass("bg-light");
+    void testAbbreviation_data();
+    void testAbbreviation();
 
-    List navList(false, "navbar-nav mr-auto");
-    navList << (ListItem().setClass("nav-item active") << Anchor("Home", QUrl("#")).setClass("nav-link"));
-    navList << (ListItem().setClass("nav-item") << Anchor("Link", QUrl("#")).setClass("nav-link"));
-    navList << (ListItem().setClass("nav-item") << Anchor("Disabled", QUrl("#")).setClass("nav-link disabled"));
-    navList << (ListItem().setClass("nav-item dropdown")
-            << Anchor("Dropdown", QUrl("#")).setClass("nav-link dropdown-toggle").setId("dropdown01").addAttribute("data-toggle", "dropdown").addAttribute("aria-haspopup", "true").addAttribute("aria-expanded", "false")
-            << (Div("dropdown-menu").addAttribute("aria-labelledby", "dropdown01")
-            << Anchor("Action", QUrl("#")).setClass("dropdown-item")
-            << Anchor("Other", QUrl("#")).setClass("dropdown-item")));
+    void testAddress_data();
+    void testAddress();
+};
 
-    doc << (Nav("navbar navbar-expand navbar-dark bg-dark") << Div("collapse navbar-collapse") << navList);
+QtHtmlTests::QtHtmlTests()
+{
 
-    Div main("container");
-
-    main << Header1("Welcome to Qt Html!");
-    main << "Text directly in the body." << LineBreak();
-
-    main << Paragraph("This is the way to go for a big text in a multi-line paragraph.");
-    main << Anchor("Google", QUrl("http://google.com")).setClass("my_style");
-
-    main << (Table().setClass("table table-hover table-sm")
-         << Caption("Table caption")
-         << (TableRow() << TableHeader("A") << TableHeader("B"))
-         << (TableRow() << TableDataCell("Cell 11") << TableDataCell("Cell 12"))
-         << (TableRow() << TableDataCell("Cell 21") << TableDataCell("Cell 22"))
-         << (TableRow() << TableDataCell("Cell 31") << (TableDataCell() << Anchor("Google", QUrl("http://www.google.com")))));
-
-    main << Label("txt1", "The text") << TextArea("textform", "This is text...").setRequired().setPlaceHolder("The text").setId("txt1");
-
-    main << Small("Copyright Serebryakov A. &copy; 2011-2023");
-
-    doc << main;
-
-    QString str;
-    QTextStream os(&str);
-    os << doc;
-
-    qInfo().noquote() << str;
-
-    return 0;
 }
+
+QtHtmlTests::~QtHtmlTests()
+{
+
+}
+
+void QtHtmlTests::initTestCase()
+{
+
+}
+
+void QtHtmlTests::cleanupTestCase()
+{
+
+}
+
+void QtHtmlTests::testAbbreviation_data()
+{
+    QTest::addColumn<QString>("content");
+    QTest::addColumn<QString>("result");
+
+    QTest::newRow("simple") << QtHtml::Abbreviation("HTML5").toString() << "<abbr>HTML5</abbr>\n";
+}
+
+void QtHtmlTests::testAbbreviation()
+{
+    QFETCH(QString, content);
+    QFETCH(QString, result);
+
+    QCOMPARE(content, result);
+}
+
+void QtHtmlTests::testAddress_data()
+{
+    QTest::addColumn<QString>("content");
+    QTest::addColumn<QString>("result");
+
+    QTest::newRow("simple") << QtHtml::Address("HTML5").toString() << "<address>HTML5</address>\n";
+}
+
+void QtHtmlTests::testAddress()
+{
+    QFETCH(QString, content);
+    QFETCH(QString, result);
+
+    QCOMPARE(content, result);
+}
+
+Q_DECLARE_METATYPE(QtHtml::Abbreviation)
+
+QTEST_APPLESS_MAIN(QtHtmlTests)
+
+#include "main.moc"
