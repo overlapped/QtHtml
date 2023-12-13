626d6a9f8c3c2beee6588f5d98ffbd1e43094e4c
aserebryakov <overlapped@gmail.com>
2023-12-13 22:23:47 +0700

    Updated section tag.

--- src/qhtmlsection.hpp
+++ src/qhtmlsection.hpp
@@ -5,6 +5,18 @@
 
 namespace QtHtml {
 
+/**
+ * \brief The Section class represents \b section HTML element.
+ *
+ * The \<section\> HTML element represents a generic standalone section of a document,
+ * which doesn't have a more specific semantic element to represent it. Sections should
+ * always have a heading, with very few exceptions.
+ *
+ * As mentioned above, \<section\> is a generic sectioning element, and should only be
+ * used if there isn't a more specific element to represent it. As an example, a navigation
+ * menu should be wrapped in a \<nav\> element, but a list of search results or a map display
+ * and its controls don't have specific elements, and could be put inside a \<section\>.
+ */
 class QTHTML_EXPORT Section : public Element
 {
 public:
