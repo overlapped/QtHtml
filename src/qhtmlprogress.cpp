11d2e2672cce90b1c7c8bda6ba25df8d0293faaa
aserebryakov <overlapped@gmail.com>
2023-12-14 00:19:26 +0700

    Updated progress tag.

--- src/qhtmlprogress.cpp
+++ src/qhtmlprogress.cpp
@@ -8,15 +8,37 @@ Progress::Progress(unsigned int value, unsigned int max, const QString &content)
     Q_ASSERT(value <= max);
     Q_ASSERT(max > 0);
 
-    addAttribute("value", value);
-    addAttribute("max", max);
+    if (value <= max)
+    {
+        addAttribute("value", value);
+    }
+    if (max > 0)
+    {
+        addAttribute("max", max);
+    }
 }
 
 Progress &Progress::setMax(unsigned int value)
 {
     Q_ASSERT(value > 0);
 
-    addAttribute("max", value);
+    if (value > 0)
+    {
+        addAttribute("max", value);
+    }
+
+    return *this;
+}
+
+Progress &Progress::setValue(unsigned int value)
+{
+    Q_ASSERT(value > 0);
+
+    if (value > 0)
+    {
+        addAttribute("value", value);
+    }
+
     return *this;
 }
 
--- src/qhtmlprogress.hpp
+++ src/qhtmlprogress.hpp
@@ -5,12 +5,49 @@
 
 namespace QtHtml {
 
+/**
+ * @brief The Progress class
+ *
+ * The \<progress\> HTML element displays an indicator showing the completion progress of
+ * a task, typically displayed as a progress bar.
+ * \note Unlike the \<meter\> element, the minimum value is always 0, and the min attribute
+ * is not allowed for the \<progress\> element.
+ */
 class QTHTML_EXPORT Progress : public Element
 {
 public:
-    Progress(unsigned int value, unsigned int max = 1, const QString &content = QString());
+    /**
+     * \brief Construct object with \c value, \c maximum value and \c content.
+     * \param[in] value - the value.
+     * \param[in] max - the maximum value.
+     * \param[in] content - the text string.
+     */
+    explicit Progress(unsigned int value, unsigned int max = 1, const QString &content = QString());
 
-    Progress& setMax(unsigned int value);
+    /**
+     * \brief Set the maximum value attribute.
+     *
+     * This attribute describes how much work the task indicated by the progress element requires.
+     * The \c max attribute, if present, must have a value greater than 0 and be a valid floating
+     * point number. The default value is \b 1.
+     *
+     * \param[in] value - the maximum value.
+     * \return this object.
+     */
+    Progress& setMax(unsigned int value = 1);
+
+    /**
+     * \brief Set value attribute.
+     *
+     * This attribute specifies how much of the task that has been completed. It must be a valid
+     * floating point number between 0 and max, or between 0 and 1 if max is omitted. If there is
+     * no value attribute, the progress bar is indeterminate; this indicates that an activity is
+     * ongoing with no indication of how long it is expected to take.
+     *
+     * \param[in] value - the value.
+     * \return this object.
+     */
+    Progress& setValue(unsigned int value = 1);
 };
 
 }
