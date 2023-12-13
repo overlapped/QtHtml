b4853345adb08716e03c3cd3450cc62831064090
aserebryakov <overlapped@gmail.com>
2023-12-13 16:31:24 +0700

    Updated span tag.

--- src/qhtmlspan.hpp
+++ src/qhtmlspan.hpp
@@ -5,10 +5,24 @@
 
 namespace QtHtml {
 
+/**
+ * @brief The Span class
+ *
+ * The \<span\> HTML element is a generic inline container for phrasing content, which does
+ * not inherently represent anything. It can be used to group elements for styling purposes
+ * (using the class or id attributes), or because they share attribute values, such as lang.
+ * It should be used only when no other semantic element is appropriate. \<span\> is very much
+ * like a \<div\> element, but \<div\> is a block-level element whereas a \<span\> is an
+ * inline-level element.
+ */
 class QTHTML_EXPORT Span : public Element
 {
 public:
-    explicit Span(const QString &content);
+    /**
+     * \brief Default constructor to create object with \b content.
+     * \param[in] content - the text string.
+     */
+    explicit Span(const QString &content = QString());
 };
 
 }
