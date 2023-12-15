95290b8af3cabb33c6720f0d0b60c79d25489674
aserebryakov <overlapped@gmail.com>
2023-12-16 00:58:03 +0700

    Updated main tag.

--- src/qhtmlmain.hpp
+++ src/qhtmlmain.hpp
@@ -5,10 +5,20 @@
 
 namespace QtHtml {
 
+/**
+ * \brief The Main class represents \b main HTML element.
+ *
+ * The \<main\> HTML element represents the dominant content of the \<body\> of a document.
+ * The main content area consists of content that is directly related to or expands upon
+ * the central topic of a document, or the central functionality of an application.
+ */
 class QTHTML_EXPORT Main : public Element
 {
 public:
-    Main();
+    /**
+     * \brief Default constructor to create object.
+     */
+    explicit Main();
 };
 
 }
